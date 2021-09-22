d93ce2c034d844129330f5567b809a689b4cd0dc
Yulia <1206bogomazovaja@gmail.com>
2021-09-22 22:07:13 +0300

    рефакторинг, readme и клавиатура

--- bot.py
+++ bot.py
@@ -1,79 +1,14 @@
-from emoji import emojize
-import ephem
-from glob import glob
 import logging
-from random import randint, choice
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+from handlers import greet_user, guess_number, send_cat_picture, user_coordinates, talk_to_me, which_constellation
 import settings
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
-
-today = '2021/09/10'
-planet_dict = {
-    'Mars': ephem.Mars(today),
-    'Venus': ephem.Venus(today),
-    'Saturn': ephem.Saturn(today),
-    'Jupiter': ephem.Jupiter(today),
-    'Neptune': ephem.Neptune(today),
-    'Uranus': ephem.Uranus(today),
-    'Mercury': ephem.Mercury(today)
-    }
-
-def get_smile(user_data):
-    if 'emoji' not in user_data:
-        smile = choice(settings.USER_EMOJI)
-        return emojize(smile, use_aliases=True)
-    return user_data['emoji']
-
-def greet_user(update, context):
-    context.user_data['emoji'] = get_smile(context.user_data)
-    update.message.reply_text(f"Здравствуй, пользователь {context.user_data['emoji']}!")
-
-def talk_to_me(update, context):
-    context.user_data['emoji'] = get_smile(context.user_data)
-    username = update.effective_user.first_name
-    text = update.message.text
-    update.message.reply_text(f"Здравствуй, {username} {context.user_data['emoji']}! Ты написал: {text}")
-
-def play_random_numbers(user_number):
-    bot_number = randint(user_number-10, user_number+10)
-    if user_number > bot_number:
-        message = f"Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!"
-    elif user_number == bot_number:
-        message = f"Ты загадал {user_number}, я загадал {bot_number}, ничья!"
-    else:
-        message = f"Ты загадал {user_number}, я загадал {bot_number}, я выиграл!"
-    return message
-    
-def guess_number(update, context):
-    if context.args:
-        try:
-            user_number = int(context.args[0])
-            message = play_random_numbers(user_number)
-        except(TypeError, ValueError):
-            message = "Введите целое число"
-    else:
-        message = "Введите целое число"
-    update.message.reply_text(message)
-
-def send_cat_picture(update, context):
-    cat_photos_list = glob('images/cat*.jp*g')
-    cat_pic_filename = choice(cat_photos_list)
-    chat_id = update.effective_chat.id
-    context.bot.send_photo(chat_id=chat_id, photo=open(cat_pic_filename, 'rb'))
-
-def which_constellation(update, context):
-    planet_name = update.message.text.split()[1]
-    ephem_body = planet_dict.get(planet_name, None)
-    if ephem_body!=None:
-        constellation = ephem.constellation(planet_dict[planet_name])
-        update.message.reply_text(constellation[1])
-    else:
-        update.message.reply_text('Я не знаю такую планету!')
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
@@ -83,6 +18,8 @@ def main():
     dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(CommandHandler("planet", which_constellation))
+    dp.add_handler(MessageHandler(Filters.regex('^(Прислать котика)$'), send_cat_picture))
+    dp.add_handler(MessageHandler(Filters.location, user_coordinates))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info ('Bot started')
